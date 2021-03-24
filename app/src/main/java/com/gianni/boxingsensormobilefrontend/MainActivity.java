123dbe2fd494c0e2fa9553807dae6ad90aa2a32b
Gianni <gianni.utoro@gmail.com>
2021-03-24 20:38:41 +0100

    Functional email/password registration page linked with firebase

--- app/src/main/java/com/gianni/boxingsensormobilefrontend/MainActivity.java
+++ app/src/main/java/com/gianni/boxingsensormobilefrontend/MainActivity.java
@@ -2,7 +2,9 @@ package com.gianni.boxingsensormobilefrontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,5 +19,21 @@ public class MainActivity extends AppCompatActivity {
 
         register =findViewById(R.id.button_register);
         login = findViewById(R.id.button_login);
+
+        register.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this , RegisterActivity.class));
+                finish();
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(MainActivity.this , LoginActivity.class));
+                finish();
+            }
+        });
     }
 }
