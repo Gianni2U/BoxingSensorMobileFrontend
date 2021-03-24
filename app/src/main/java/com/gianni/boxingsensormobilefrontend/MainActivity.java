58711b891ceab112cc55fcc469f7d89a836f856c
Gianni <gianni.utoro@gmail.com>
2021-03-24 21:57:08 +0100

    Attempts at database communication, code commented out at MainActivity.java

--- app/src/main/java/com/gianni/boxingsensormobilefrontend/MainActivity.java
+++ app/src/main/java/com/gianni/boxingsensormobilefrontend/MainActivity.java
@@ -2,6 +2,7 @@ package com.gianni.boxingsensormobilefrontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -9,10 +10,16 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button logout;
+    //private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    //private DatabaseReference myRef = database.getReference("UserData");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +33,21 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 FirebaseAuth.getInstance().signOut();
                 Toast.makeText(MainActivity.this, "Logged out", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(MainActivity.this , StartActivity.class));
+                startActivity(new Intent(MainActivity.this, StartActivity.class));
             }
         });
+
+      //  myRef.child("UID").setValue("BE000001");
+        // FirebaseDatabase.getInstance().getReference().child("UserData").child("UID").setValue("BE190U98");
+
+      /*  HashMap<String, Object>map = new HashMap<>();
+        map.put("Name" , "Gianni Utoro");
+        map.put("Email", "s109211@ap.be");
+        map.put("Trainingdata", "Basically Mike Tyson");
+
+        FirebaseDatabase.getInstance().getReference().child("UserData").updateChildren(map);
+            */
+
+
     }
 }
